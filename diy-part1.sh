6579c4cb3144080b11940ac1bd51b1a117a1c42a
Kokin <necoic@gmail.com>
2022-10-04 08:32:50 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -15,5 +15,5 @@ sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
 # Add a feed source
 echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
-
+echo 'src-git openclash https://github.com/vernesong/OpenClash.git' >>feeds.conf.default
 git clone https://github.com/thinktip/luci-theme-neobird.git package/lean/luci-theme-neobird
